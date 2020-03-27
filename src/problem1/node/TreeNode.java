5ae94b57c6fe44a3f112f0e4013e1d15bedb4fc2
Abhishek chaudhary <abhishek.chaudhary_cs18@gla.ac.in>
2020-03-27 23:08:13 +0530

    Decelearing the getter and setter method

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,4 +11,28 @@ public class TreeNode {
     private TreeNode right;
     private TreeNode left;
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
 }
