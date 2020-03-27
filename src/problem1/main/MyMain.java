36e185565997a1f8e6672e7872e1b29adcca3e81
Abhishek chaudhary <abhishek.chaudhary_cs18@gla.ac.in>
2020-03-27 23:45:41 +0530

    deceleared the main class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,22 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(20);
+        obj.insert(22);
+        obj.insert(21);
+        obj.insert(10);
+        obj.insert(11);
+        obj.insert(5);
+        obj.insert(60);
+        obj.insert(59);
+        System.out.print("Left Child nodes are: ");
+        obj.printLeftNode(obj.root);
+        System.out.println();
+        System.out.println("No of nodes that doesnt have a left chuld node are: " + obj.getCount());
+    }
 }
