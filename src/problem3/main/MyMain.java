64194b44b215c280e458fa884821c1a449ecb37e
Abhishek chaudhary <abhishek.chaudhary_cs18@gla.ac.in>
2020-03-28 14:53:24 +0530

    problem3 statement main is created

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Abhishek", 01);
+        obj.insert("Abhi", 02);
+        obj.insert("Chaudhary", 20);
+        obj.insert("Aman", 05);
+        obj.insert("Udit", 53);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
